2111c7c2511d453366cbcc86d2e92568f88ced19
Mila3008 <camilapassosdafiuza@gmail.com>
2025-06-02 09:07:32 -0300

    tipos de dados

--- models/produtos.cs
+++ models/produtos.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApplication1.models
+﻿using Microsoft.EntityFrameworkCore.Storage.Json;
+
+namespace WebApplication1.models
 {
 	public class produtos : Base
 	{
@@ -7,5 +9,16 @@
 		public int  Quantidade { get; set; }
 		public decimal Valor { get; set; }
 
-	}
+
+		// tipos de dados 
+		string nome = "Etec"; // tipo de ref
+		char letra = 'e';
+
+		int valor = 10; // numeros inteiros
+		long valor1 = 10L;
+		byte valorB = 127;
+
+
+		double valor2 = 10.0;
+		float valor3 = 10.5F;
 }
